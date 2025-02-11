ea6b7f4238572b383048c6c79cde3f0a28574f9c
vivianbarclay <vivian.m.barclay@gmail.com>
2025-02-11 22:33:57 +0000

    rails static dice roll vb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
-  skip_forgery_protection
+
+  def home
+  end
+
+  def dice_2_6
+    @title = "2d6"
+    @results = [rand(1..6), rand(1..6)]
+    render "application/roll"
+  end
+
+  def dice_2_10
+    @title = "2d10"
+    @results = [rand(1..10), rand(1..10)]
+    render "application/roll"
+  end
+
+  def dice_1_20
+    @title = "1d20"
+    @results = [rand(1..20)]
+    render "application/roll"
+  end
+
+  def dice_5_4
+    @title = "5d4"
+    @results = Array.new(5) { rand(1..4)}
+    render "application/roll"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
+  root 'application#home'          # Home page
 
-  # get "/your_first_screen" => "pages#first"
+  get '/dice/2/6', to: 'application#dice_2_6'
+  get '/dice/2/10', to: 'application#dice_2_10'
+  get '/dice/1/20', to: 'application#dice_1_20'
+  get '/dice/5/4', to: 'application#dice_5_4'
   
 end
